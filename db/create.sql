88782ef7d7c042250281ea9bce40b8fdf1e54761
Nathalieh2010 <93260498+Nathalieh2010@users.noreply.github.com>
2023-01-23 10:56:52 +0000

    producten verandert

--- db/create.sql
+++ db/create.sql
@@ -27,12 +27,13 @@ CREATE TABLE eigenschappen (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, price) values ('Armband', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id.', '816905633-0', 10.5);
-insert into products (name, description, code, price) values ('Post Mortem', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3', 11);
-insert into products (name, description, code, price) values ('Scarlet and the Black, The', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 13.5);
-insert into products (name, description, code, price) values ('Aquí llega Condemor, el pecador de la pradera', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7', 13.5);
-insert into products (name, description, code, price) values ('Kiss for Corliss, A (Almost a Bride)', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 14);
-insert into products (name, description, code, price) values ('Velvet Goldmine', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14);
+insert into products (name, description, code, price) values ('roze quarts steen , `de roze quarts is een troebele steen met een roze achtige kleur. de steen staat voor liefde en bescherming.' , '007301' , 12);
+insert into products (name, description, code, price) values ('fluoriet , floriet is een edelsteen met een beschermende werking. met deze steen krijg je een betere zelfvertrouwen en brengt structuur in je leven.' '007302' , 12);
+insert into products (name, description, code, price) values ('obsidaan , obsidaan is een donker kleurige steen. vaak is deze kleur zwart of paars. deze steen heeft een werking van persoonlijke groei. door deze steen komt de waarheid naar boven.' , '007303' , 12);
+insert into products (name, description, code, price) values ();
+insert into products (name, description, code, price) values ();
+insert into products (name, description, code, price) values ();
+
 
 
 insert into eigenschappen (grootte, kleur, functie) values ('3 cm, roze', 'liefde en bescherming', 1);
